eb92684d15248588fcc243814fe238a4b73c8672
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2022-03-09 11:11:08 +0200

    Add an example test

---
+++ AMRL/Environment/test_episode_memory.py
@@ -0,0 +1,15 @@
+from episode_memory import Episode_Memory
+
+
+class TestEpisode_Memory():
+
+    def test_init(self):
+
+       em = Episode_Memory()
+       em.memory_init(episode=1)
+
+       assert(em.episode == 1)
+
+
+
+
