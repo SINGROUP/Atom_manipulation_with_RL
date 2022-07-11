f0fd760d4a2917c37c8051eeb9e16a5019064468
akipnis <abekipnis@yahoo.com>
2022-07-11 10:53:51 +0300

    updating setup.py and requirements to fix pywin32 for readthedocs

--- setup.py
+++ setup.py
@@ -7,13 +7,22 @@ requirements = [ line.strip() for line in open(requirementstxt, "r") if line.str
 setup(
     name='AMRL',
     version='0.0.0',
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     url="https://atom-manipulation-with-rl.readthedocs.io/en/latest/",
     install_requires=requirements,
+
+    # in order to make the documentation compile on readthedocs
+    # we need to make pywin32 an 'extra' package since rtd runs on linux
+    # thus to pip install and include pywin32 in requirements
+    # we have to run ´pip install .[run]´
+    extras_require={
+        'run': [
+            'pywin32'
+        ]
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "Development Status :: 3 - Alpha",
         "Operating System :: Microsoft :: Windows",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
-
     ])
